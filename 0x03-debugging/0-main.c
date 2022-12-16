fd40f22d85068730d8685cc72bd69f821cf766b4
Victorcosmas <victorcosmas@gmsil.com>
2022-12-16 08:01:33 -0800

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
+#include <stdio>
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative 
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
