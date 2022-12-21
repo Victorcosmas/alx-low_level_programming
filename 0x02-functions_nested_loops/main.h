a48ce936b9eb97f10105b7439f18b44dff11d525
Victorcosmas <victorcosmas@gmsil.com>
2022-12-21 07:44:47 -0800

    task 12

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * main.h file
+ * Header file that contains the declaration of our functions
+ */
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
